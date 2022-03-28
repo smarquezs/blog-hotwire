02777efe8668fd819b946a6be996b7392a7bb15c
Sergio Márquez <smarquezs@gmail.com>
2022-03-28 17:35:43 -0300

    Register stimulus controller & use postcss.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import PostsController from "./posts_controller.js"
+application.register("posts", PostsController)
