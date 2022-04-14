2e349ad23eaf496757606b938dca02b421ddadb5
Sergio Márquez <smarquezs@gmail.com>
2022-04-14 11:37:27 -0400

    Add search feature.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
+  include PgSearch::Model
+
+  belongs_to :associated_author, class_name: :Author, foreign_key: :author_id
+
   after_create_commit { broadcast_append_to 'posts' }
   after_update_commit { broadcast_replace_to 'posts' }
   after_destroy_commit { broadcast_remove_to 'posts' }
+
+  pg_search_scope :search,
+                  against: { title: 'A', body: 'B' },
+                  associated_against: { associated_author: { name: 'A', city: 'B' } },
+                  using: { tsearch: { prefix: true } }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_202047) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_144600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_30_202047) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "posts", "authors"
 end
